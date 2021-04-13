a4a0d301e750c3ffc6de31174c15138bf96ebc5f
Trivenk Agarwal <60007440+Trivenk@users.noreply.github.com>
2021-04-13 15:20:16 +0530

    Update Node.java

--- src/myinterface/Node.java
+++ src/myinterface/Node.java
@@ -2,4 +2,10 @@ package myinterface;
 
 public interface Node<E extends Comparable<E>> {
     //marker interface
+    public E getData();
+    public void setData(E data);
+    public Node<E> getLeft();
+    public Node<E> getRight();
+    public void setLeft(Node<E> left);
+    public void setRight(Node<E> right);
 }

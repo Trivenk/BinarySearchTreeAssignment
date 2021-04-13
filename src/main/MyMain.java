2a7c5e9ebc4746aa8af792d5ae38e3529ec80830
Trivenk Agarwal <60007440+Trivenk@users.noreply.github.com>
2021-04-13 15:28:13 +0530

    Update MyMain.java

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -4,5 +4,17 @@ public class MyMain {
     public static void main(String[] args) {
         //create binary search tree
         // and perform insert, search, traversal, delete
+        MyBinarySearchTree<Integer> mt = new MyBinarySearchTree<>();
+        mt.insert(5);
+        mt.insert(2);
+        mt.insert(8);
+        mt.insert(1);
+        mt.insert(4);
+        mt.insert(11);
+        mt.insert(3);
+        mt.inOrder(mt.getRoot());
+        System.out.println();
+        System.out.println(mt.search(15));
+        System.out.println(mt.height(mt.getRoot()));
     }
 }
